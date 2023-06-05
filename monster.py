68f23307f8043db6aa01f6e0ad065fc243902df1
Psuedomonas <zehm0007@d.umn.edu>
2023-06-05 15:37:21 -0500

    Update monster.py
    upload monster.py for version 0.2.2

--- monster.py
+++ monster.py
@@ -3,25 +3,26 @@ By Nicholas Zehm
 2021-3-17
 Monster class
 filename: monster.py
-for MonsterBattle-Console.py version  0.1.3.1 (2021-3-19)
-version 1.1 (2021-3-19)
-Notes:
-The advantage, as far as I can see, for wrapping variables in methods is protecting client input
-Otherwise, with some code changes, I could simply use a dictionary. This would use a lot less code, no idea
-how it would effect execution time.
+for MonsterBattle-Console-exp/MonsterBattleDome.py 
+version  0.2.0 (2022-10-25)
 '''
 
+#Monsters in the pen
 pen = {}
 
+#Monsters in the combat areana
+to_arena = []
 
 class Monster:
     '''Initialize the monster'''
-    def __init__(self, health, stamina, exp, lvl):
+    def __init__(self, health, stamina, exp, lvl, attack_skill, defense_skill):
         self.health = health
         self.exp = exp
         self.maxhealth = health
         self.stamina = stamina
         self.maxstamina = stamina
+        self.attack_skill = attack_skill
+        self.defense_skill = defense_skill
         self.level = lvl
 
     #Accessors
@@ -33,19 +34,25 @@ class Monster:
 
     def getMaxHealth(self):
         return self.maxhealth
-        
+
     def getStamina(self):
         return self.stamina
-    
+
     def getMaxStamina(self):
         return self.maxstamina
-    
+        
+    def getAttackSkill(self):
+        return self.attack_skill
+
+    def getDefenseSkill(self):
+        return self.defense_skill
+
     def getLevel(self):
         return self.level
 
     #Mutators
     def setHealth(self, health):
-        if health < 0: 
+        if health < 0:
             health = 0
         self.health = health
 
@@ -61,12 +68,22 @@ class Monster:
 
     def setStamina(self, stamina):
         self.stamina = stamina
-    
+
     def setMaxStamina(self, stamina):
         if stamina < 0:
             stamina = 0
         self.maxstamina = stamina
         
+    def setAttackSkill(self, attack_skill):
+        if attack_skill < 0:
+            attack_skill = 0
+        self.attack_skill = attack_skill
+        
+    def setDefenseSkill(self, defense_skill):
+        if defense_skill < 0:
+            defense_skill = 0
+        self.defense_skill = defense_skill
+
     def setLevel(self, level):
         if level < 0:
             level = 0
